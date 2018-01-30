0f431ccbe9597558ff269987a01e7813e650c9c5
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-30 14:26:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
       end
     elsif ((side_2 == side_3 && side_2 != side_1 && side_3 != side_1) || (side_1 == side_3 && side_1 != side_2 && side_2 != side_3) || (side_1 == side_2 && side_1 != side_3 && side_2 != side_3))
       :isosceles
-    elsif ((side_1 == side_2 ) && (side_1 == side_3)
+    elsif (side_1 == side_2 ) && (side_1 == side_3)
       #here we can't simply say if 1==2 ==3 because it must only be in sets of two
       #so we do something like if 1==2 && 1==3 instead
        :equilateral
