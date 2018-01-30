1c181ec7f83a19b30efa5ac3206de270f831030d
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-30 14:25:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,14 +12,18 @@ class Triangle
 
   def kind
       if ((side_1 <= 0) || (side_2 <= 0) || (side_3 <= 0) || (side_2 + side_3 <= side_1) || (side_1 + side_3 <= side_2) || (side_1 + side_2 <= side_3))
+        #for complex booleans, use parenthesis.
       begin
         raise TriangleError
       end
     elsif ((side_2 == side_3 && side_2 != side_1 && side_3 != side_1) || (side_1 == side_3 && side_1 != side_2 && side_2 != side_3) || (side_1 == side_2 && side_1 != side_3 && side_2 != side_3))
       :isosceles
     elsif ((side_1 == side_2 ) && (side_1 == side_3) && (side_2 == side_3))
+      #here we can't simply say if 1==2 ==3 because it must only be in sets of two
+      #so we do something like if 1==2 && 2==3 && 1==3 instead
        :equilateral
     else
+      #no need to specify what scalene is if all others are accounted for
        :scalene
      end
   end
